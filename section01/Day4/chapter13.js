96445c39ad657d014849077af67216572247dea3
tacoringo <130815277+TakHoYeong@users.noreply.github.com>
2026-01-15 21:36:15 +0900

    react_training 4일차
    콜백함수, 스코프

---
+++ section01/Day4/chapter13.js
@@ -0,0 +1,22 @@
+// 1. 콜백함수
+function main(value){
+    console.log(value);
+}
+
+function sub(){
+    console.log("i am sub");
+}
+
+main(sub);
+
+
+// 2. 콜백함수의 활용
+function repeat(count, callback){
+    for(let idx = 1; idx <= count; idx++){
+        console.log(idx);
+    }
+}
+
+repeat(5, function(idx) {
+    console.log(idx);
+});
