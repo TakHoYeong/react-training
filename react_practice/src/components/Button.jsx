cb717d93bd571bab9aa75d8ab6e9bcda77290068
tacoringo <130815277+TakHoYeong@users.noreply.github.com>
2026-02-13 18:42:39 +0900

    이벤트 처리하기
    react_training 25일차

--- react_practice/src/components/Button.jsx
+++ react_practice/src/components/Button.jsx
@@ -1,8 +1,19 @@
 const Button = ({ children, text, color = "black" }) => {
   // 19버전 이후 기본값 설정 구조분해할당 사용
+  // 합성이벤트 객체 : 브라우저 별 스펙이 달라서 발생하는 문제(Cross Browsing Issue)
+  //                  때문에 생겨난 통합 규격
+  
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
 
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton}
+      // onMouseEnter = {onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
