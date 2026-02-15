c01636dd959aeb03b49605add52f610776b36a9d
tacoringo <130815277+TakHoYeong@users.noreply.github.com>
2026-02-15 23:46:11 +0900

    State로 상태관리하기
    react_training 26일차

--- react_practice/src/App.jsx
+++ react_practice/src/App.jsx
@@ -1,26 +1,31 @@
 import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Button from "./components/Button";
+import { useState } from "react";
 
+// 리액트는 state의 값이 바뀌었을 때 리렌더링된다.
+// 그래서 변수의 값을 바꿔주도록 코드를 짜면 화면엔 변화가 없다.
 function App() {
-  const buttonProps = {
-    //스프레드 연산 사용
-    text: "카페",
-    color: "red",
-    a: "1",
-    b: "2",
-    c: 3,
-  };
+  const [count, setCount] = useState(0);
+  // state에는 값이 들어있고 setState에는 함수가 들어있다.
+  const [light, setLight] = useState("OFF");
 
-  // div 자식요소는 props에 children이라는 키값으로 전달해준다.
   return (
     <>
-      <Button text={"메일"} color={"yellow"} a={1} />
-      <Button {...buttonProps} />
-      <Button text={"블로그"} />
-      <div>자식 요소</div>
+    <div>
+      <h1>{light}</h1>
+      <button onClick={() => {
+        setLight(light === "ON" ? "OFF" : "ON");
+      }}>
+        {light === "ON" ? "끄기" : "켜기"}
+      </button>
+    </div>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={()=>{
+        setCount(count + 1);
+      }}>
+        +
+      </button>
+    </div>
     </>
   );
 }
