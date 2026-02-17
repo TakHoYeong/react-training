fb6102b862a091183b6f608dee1942e823d1f74c
tacoringo <130815277+TakHoYeong@users.noreply.github.com>
2026-02-17 12:47:41 +0900

    State로 사용자 입력관리하기1

--- react_practice/src/App.jsx
+++ react_practice/src/App.jsx
@@ -1,8 +1,5 @@
 import "./App.css";
-import { useState } from "react";
-
-import Bulb from "./components/Bulb";
-import Counter from "./components/Counter";
+import Register from "./components/Register";
 // 리액트는 state의 값이 바뀌었을 때 리렌더링된다.
 // 그래서 변수의 값을 바꿔주도록 코드를 짜면 화면엔 변화가 없다.
 // 그 외에도 props의 값이 바뀌거나 부모 컴포넌트의 값이 바뀌면 자식 컴포넌트를 리렌더링한다.
@@ -10,8 +7,7 @@ import Counter from "./components/Counter";
 function App() {
   return (
     <>
-      <Bulb />
-      <Counter />
+      <Register />
     </>
   );
 }
