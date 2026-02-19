8f8c0a00d542cee5222be443f3eab8590609c82a
tacoringo <130815277+TakHoYeong@users.noreply.github.com>
2026-02-19 21:50:58 +0900

    useRef
    react_training 30일차

--- react_practice/src/components/Register.jsx
+++ react_practice/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 // 간단한 회원가입 폼
 // 이름, 생년월일, 국적, 자기소개
 
@@ -10,18 +10,36 @@ const Register = () => {
     bio: "",
   });
 
+  const countRef = useRef(0);
+  const inputRef = useRef();
+  // Ref를 사용하지않고 javascript 코드를 쓰면 리렌더링이 되기 때문에 잠재적인 오류가 발생한다.
+  // 변수를 렌더링 바깥에 선언해두면 되지않나? 싶지만
+  // 그렇게 되면 App.jsx에서 두번 렌더링을 하는경우 치명적이다.
+  // 첫번째 input에서 count된 것이 초기화 되지않고 두번째 input까지 영향을 미치기 때문
+  let count = 0;
+
   const onChange = (e) => {
-    console.log(e.target.name, e.target.value);
+    // countRef.current++;
+    count++;
+    console.log(count);
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
   };
 
+  const onSubmit = () => {
+    if (input.name === "") {
+      // 이름을 입력하는 DOM 요소 포커스
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <div>
       <div>
         <input
+          ref={inputRef}
           name="name"
           value={input.name}
           onChange={onChange}
@@ -47,6 +65,7 @@ const Register = () => {
       <div>
         <textarea name="bio" value={input.bio} onChange={onChange}></textarea>
       </div>
+      <button onClick={onSubmit}>제출</button>
     </div>
   );
 };
