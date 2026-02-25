7df24ae54f194cef46c8c8fd51e3201f626ead3b
tacoringo <130815277+TakHoYeong@users.noreply.github.com>
2026-02-25 20:40:22 +0900

    카운터 앱 완료
    react_training 34일차

--- countApp/src/components/Viewer.jsx
+++ countApp/src/components/Viewer.jsx
@@ -1,8 +1,12 @@
-const Viewer = () => {
-    return <div>
+const Viewer = ({count}) => {
+
+    return (
+    <div>
         <div>현재 카운트 :</div>
-        <h1>0</h1>
-    </div>;
+        <h1>{count}</h1>
+    </div>
+    );
+        
 }
 
 export default Viewer;
