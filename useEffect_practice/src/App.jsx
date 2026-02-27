d3a4e7a67f96bab45eea9ea44794392b253a98ea
tacoringo <130815277+TakHoYeong@users.noreply.github.com>
2026-02-27 18:38:13 +0900

    useEffect로 라이프사이클 제어, React 개발자도구 확장프로그램
    react_training 36일차

--- useEffect_practice/src/App.jsx
+++ useEffect_practice/src/App.jsx
@@ -1,15 +1,35 @@
 import "./App.css";
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
-import { useState, useEffect } from "react";
+import Even from "./components/Even";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
 
+  const isMount = useRef(false);
   // useEffect 대신 onClick이벤트에 console 출력하면 안되나?
   // -> setCount가 비동기이기 때문에 함수의 완료가 뒤늦게 되기 때문에
   // 시점이 맞지않아서 오류가 발생할 수도 있다.
+
+  // 1. Mount
+  useEffect(() => {
+    console.log("mount");
+  }, []);
+
+  // 2. Update
+  useEffect(() => {
+    if (!isMount.current){
+      isMount.current = true;
+      return;
+    }
+    console.log("update");
+  });
+
+  // 3. UnMount
+
+
   useEffect(() => {
     console.log(`count:${count} / input: ${input}`);
   }, [count, input]);
@@ -32,6 +52,7 @@ function App() {
       </section>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />
