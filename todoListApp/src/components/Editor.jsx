1deaad91b91abee75b9ff5535e96d42ce18b8c05
tacoringo <130815277+TakHoYeong@users.noreply.github.com>
2026-03-05 00:32:00 +0900

    기능 구현

--- todoListApp/src/components/Editor.jsx
+++ todoListApp/src/components/Editor.jsx
@@ -1,13 +1,42 @@
 import "./Editor.css";
+import { useState, useRef } from "react";
 
-const Editor = () => {
-    return (
-        <div className="Editor">
-            <input placeholder="새로운 Todo..."/>
-            <button>추가</button>
-        </div>
-    )
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
 
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      //엔터키 인 것 같다.
+      onSubmit();
+    }
+  };
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  };
+
+  return (
+    <div className="Editor">
+      <input
+        onKeyDown={onKeyDown}
+        ref={contentRef}
+        value={content}
+        onChange={onChangeContent}
+        placeholder="새로운 Todo..."
+      />
+      <button onClick={onSubmit}>추가</button>
+    </div>
+  );
 };
 
 export default Editor;
